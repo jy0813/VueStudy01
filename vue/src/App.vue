8ad1c8c59d3bb7f0f0c0aa1406e3326acda7390e
jy0813 <busbug3@gmail.com>
2022-04-26 16:09:51 +0900

    feat: cli 수정

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader></AppHeader>
+    <div>{{ str }}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AppHeader,
+  },
+  data() {
+    return {
+      str: "hi",
+    };
+  },
+};
 </script>
 
 <style>
