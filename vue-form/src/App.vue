a78809c2a2dd6aeb5ed70a48365186514a882984
jy0813 <75197188+jy0813@users.noreply.github.com>
2022-05-09 21:21:41 +0900

    feat: loginForm 생성

---
+++ vue-form/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log(this.username, this.password);
+      let url = "https://jsonplaceholder.typicode.com/users";
+      let data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
